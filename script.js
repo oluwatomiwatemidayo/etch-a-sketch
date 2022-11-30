fa5474cd2657837d510e8137bbae3b69706d6b45
Tomiwa <72953345+oluwatomiwatemidayo@users.noreply.github.com>
2022-11-30 10:48:19 +0100

    Added more grids

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const container = document.querySelector('#container');
 
-for (let i = 0; i < 15; i++) {
+for (let i = 0; i < 99; i++) {
     const gridDiv = document.createElement('div');
     gridDiv.classList.add('gridDiv');
     gridDiv.style.cssText = 'border: 1px solid red; height: 16px; width: 16px;';
