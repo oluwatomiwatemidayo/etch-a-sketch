8db4a6636d787af1d56a1d110d38c7a729bd8bc6
Tomiwa <72953345+oluwatomiwatemidayo@users.noreply.github.com>
2022-11-30 10:40:46 +0100

    Added border color with height and width to the grids

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const container = document.querySelector('#container');
 
-for (let i = 0; i < 9; i++) {
+for (let i = 0; i < 15; i++) {
     const gridDiv = document.createElement('div');
     gridDiv.classList.add('gridDiv');
+    gridDiv.style.cssText = 'border: 1px solid red; height: 16px; width: 16px;';
     container.append(gridDiv);
 }
 console.log(container);
