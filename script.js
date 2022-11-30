20feb4c6e01e83441570bf3f38bb63b6b3778126
Tomiwa <72953345+oluwatomiwatemidayo@users.noreply.github.com>
2022-11-30 14:25:35 +0100

    Change from onclick to method mouseover to change bgcolor

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const container = document.querySelector('#container');
 
-for (let i = 0; i < 99; i++) {
+for (let i = 0; i < 100; i++) {
     const gridDiv = document.createElement('div');
     gridDiv.classList.add('gridDiv');
-    gridDiv.style.cssText = 'border: 1px solid red; height: 16px; width: 16px;';
-    gridDiv.addEventListener('click', () => {
-        gridDiv.style.backgroundColor = 'black';
+    gridDiv.style.cssText = 'height: 16px; width: 16px;';
+    gridDiv.addEventListener('mouseover', () => {
+        gridDiv.style.backgroundColor = 'red';
     });
     container.append(gridDiv);
 }
