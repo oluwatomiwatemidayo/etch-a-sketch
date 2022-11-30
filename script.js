109bdcf466024be9c1486aecff14fb9f0d3eb63e
Tomiwa <72953345+oluwatomiwatemidayo@users.noreply.github.com>
2022-11-30 11:34:51 +0100

    Added EventListener to change background colors on grids

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ for (let i = 0; i < 99; i++) {
     const gridDiv = document.createElement('div');
     gridDiv.classList.add('gridDiv');
     gridDiv.style.cssText = 'border: 1px solid red; height: 16px; width: 16px;';
+    gridDiv.addEventListener('click', () => {
+        gridDiv.style.backgroundColor = 'black';
+    });
     container.append(gridDiv);
 }
 console.log(container);
