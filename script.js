6a5d8f6d64217e55b5e67cc222301eab4dd494f7
Tomiwa <72953345+oluwatomiwatemidayo@users.noreply.github.com>
2022-12-07 13:32:08 +0100

    Added a function to create the number of grid a user wants

--- script.js
+++ script.js
@@ -9,4 +9,19 @@ for (let i = 0; i < 256; i++) {
     });
     container.append(gridDiv);
 }
-console.log(container);
+
+function createGrid() {
+    const container = document.querySelector('#container');
+    let numberOfGrids = prompt("Enter the number of grids you want between 1 - 100:");
+
+    if (numberOfGrids < 100 && numberOfGrids > 0) {
+        for (i = 0; i < numberOfGrids * numberOfGrids; i++) {
+            const gridDiv = document.createElement('div');
+            gridDiv.classList.add('gridDiv');
+            gridDiv.addEventListener('mouseover', () => {
+                gridDiv.style.backgroundColor = 'red';
+            });
+            container.append(gridDiv);
+        }
+    }
+}
