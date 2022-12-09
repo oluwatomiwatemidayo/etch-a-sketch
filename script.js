0acf6846ed6c33ff73c3a7ec2e664d9ce1d14f3c
Tomiwa <72953345+oluwatomiwatemidayo@users.noreply.github.com>
2022-12-09 14:23:07 +0100

    Created a function to remove old grid and replace it with a new one

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const container = document.querySelector('#container');
 let numberOfGrids;
 
-for (let i = 0; i < 256; i++) {
+for (let i = 0; i < 16 * 16; i++) {
     const gridDiv = document.createElement('div');
     gridDiv.classList.add('gridDiv');
+    gridDiv.classList.add('grid');
     gridDiv.addEventListener('mouseover', () => {
         gridDiv.style.backgroundColor = 'red';
     });
@@ -12,7 +13,7 @@ for (let i = 0; i < 256; i++) {
 
 function createGrid() {
     const container = document.querySelector('#container');
-    let numberOfGrids = prompt("Enter the number of grids you want between 1 - 100:");
+    numberOfGrids = prompt("Enter the number of grids you want between 1 - 100:");
 
     if (numberOfGrids < 100 && numberOfGrids > 0) {
         for (i = 0; i < numberOfGrids * numberOfGrids; i++) {
@@ -23,5 +24,15 @@ function createGrid() {
             });
             container.append(gridDiv);
         }
+    } else {
+        alert("Only numbers between 1 - 100 is allowed!");
     }
+}
+
+function newGrid() {
+    const gridDiv = document.querySelectorAll('.gridDiv');
+    gridDiv.forEach(gridDiv => {
+        gridDiv.remove();
+    });
+    createGrid();
 }
